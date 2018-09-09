32c2a2436234559a228b7f0a4421bae8ca51b28a
razvarma <rajeshvarmadatla@gmail.com>
2018-09-09 11:42:38 +1000

    added chromedriver for linux

--- src/main/java/com/mindoula/util/TestBase.java
+++ src/main/java/com/mindoula/util/TestBase.java
@@ -52,7 +52,7 @@ public void init() {
 		{
 		String browsername = prop.getProperty("browser");
 		if (browsername.toUpperCase().equals("CHROME")) {
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//main//resources//chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//main//resources//chromedriver");
 			driver = new ChromeDriver();
 		} else if (browsername.toUpperCase().equals("FIREFOX")) {
 			driver = new FirefoxDriver();
